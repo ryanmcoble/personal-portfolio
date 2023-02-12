de64ad207c556989a5a0c2ba052a47c75b341dc3
Ryan Coble <ryan.m.coble@gmail.com>
2023-02-12 15:31:05 -0500

    Latest updates. Basic layout is done with some animations.

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,75 +1,107 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import Image from 'next/image'
-import styles from './layout.module.scss'
-import utilStyles from '../styles/utils.module.scss'
 import Link from 'next/link'
+import gsap from 'gsap'
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
+import {ScrollToPlugin} from 'gsap/ScrollToPlugin'
 
 const name = 'Ryan Coble'
 export const siteTitle = 'Ryan Coble\'s Portfolio'
 
 export default function Layout({
-  children,
-  home
+    children,
+    home
 }: {
-  children: React.ReactNode
-  home?: boolean
+    children: React.ReactNode
+    home?: boolean
 }) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/ryan.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <Image
-                priority
-                src="/images/ryan.jpg"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt={name}
-              />
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">← Back to home</Link>
+
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.registerPlugin(ScrollToPlugin);
+
+    return (
+        <div className='container'>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                name="description"
+                content="Ryan Coble's Portfolio"
+                />
+                <meta
+                property="og:image"
+                content={`https://og-image.vercel.app/${encodeURI(
+                    siteTitle
+                )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                />
+                <meta name="og:title" content={siteTitle} />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+
+            {/*  Site header */}
+            <div className='nav-container'>
+                <header>
+                    <a href='/' className='logo'>Ryan Coble</a>
+
+                    <nav>
+                        <ul>
+                            <li><a href='#'>My work</a></li>
+                            <li><a href='#'>My skills</a></li>
+                            <li><a href='#'>Hire me</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <div className='social-header'>
+                    <ul>
+                        <li><a target='_blank' href='https://www.github.com/ryanmcoble'><i className="fa-brands fa-github"></i></a></li>
+                        <li><a target='_blank' href='https://www.linkedin.com/in/rcoble/'><i className="fa-brands fa-linkedin"></i></a></li>
+                        <li><a href='#'><i className="fa-brands fa-twitter"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            {/* <header className={styles.header}>
+                {home ? (
+                <>
+                    <Image
+                    priority
+                    src="/images/ryan.jpg"
+                    className={utilStyles.borderCircle}
+                    height={144}
+                    width={144}
+                    alt={name}
+                    />
+                    <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                </>
+                ) : (
+                <>
+                    <Link href="/">
+                    <Image
+                        priority
+                        src="/images/ryan.jpg"
+                        className={utilStyles.borderCircle}
+                        height={108}
+                        width={108}
+                        alt={name}
+                    />
+                    </Link>
+                    <h2 className={utilStyles.headingLg}>
+                    <Link href="/" className={utilStyles.colorInherit}>
+                        {name}
+                    </Link>
+                    </h2>
+                </>
+                )}
+            </header> */}
+            <main>{children}</main>
+            {!home && (
+                <div className='backToHome'>
+                <Link href="/">← Back to home</Link>
+                </div>
+            )}
+
+
+            <Script src="https://kit.fontawesome.com/1fe7179e06.js" crossOrigin="anonymous"></Script>
         </div>
-      )}
-    </div>
-  )
+    )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,44 +5,213 @@ import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 import { GetStaticProps } from 'next'
+import gsap from 'gsap'
+import { useEffect, useRef } from 'react'
 
 export default function Home({
-  allPostsData
+    allPostsData
 }: {
-  allPostsData: {
-    date: string
-    title: string
-    id: string
-  }[]
+    allPostsData: {
+        date: string
+        title: string
+        id: string
+    }[]
 }) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Building Beautifully Designed Software Solutions</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  )
+
+    const contentRef = useRef(null);
+    const headerRef = useRef(null);
+    const meetRef = useRef(null);
+    const scrollRef = useRef(null);
+    const heroDesignRef = useRef(null);
+
+    useEffect(() => {
+        const tl = gsap.timeline();
+        tl.from(contentRef.current, {
+            y: '-30%',
+            opacity: 0,
+            duration: 2,
+            ease: 'power4.out',
+        });
+
+        tl.from(headerRef.current, {
+            opacity: 0,
+            y: -50,
+            ease: 'power4.out',
+            stagger: 0.3,
+            duration: 2,
+        }, '-=1.5');
+        tl.from(meetRef.current, {
+            opacity: 0,
+            y: -50,
+            ease: 'power4.out',
+            stagger: 0.3,
+            duration: 2,
+        }, '-=1.5');
+        tl.from(scrollRef.current, {
+            opacity: 0,
+            y: -50,
+            ease: 'power4.out',
+            stagger: 0.3,
+            duration: 2,
+        }, '-=1.5');
+
+        tl.from(heroDesignRef.current, {
+            opacity: 0,
+            y: 50,
+            ease: 'power4.out',
+            duration: 1
+        }, '-=2');
+    }, []);
+
+    return (
+        <Layout home>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+
+            {/* Hero */}
+            <div className='hero'>
+                <div ref={contentRef} className='content'>
+                    <h1 ref={headerRef} className=''>Thoughtfully Crafted Software Solutions</h1>
+                    <div ref={meetRef} className='meet'>
+                        <span>👇</span>
+                        <p>Meet Ryan Coble</p>
+                    </div>
+
+                    {/* Scroll icon svg */}
+                    {/* <svg className='scroll' width="32" height="61" viewBox="0 0 32 61" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="2" y="2" width="28" height="57" rx="14" stroke="white" stroke-width="4"/>
+                    <path className='circle' d="M25 15C25 19.9706 20.9706 24 16 24C11.0294 24 7 19.9706 7 15C7 10.0294 11.0294 6 16 6C20.9706 6 25 10.0294 25 15Z" fill="#D9D9D9"/>
+                    </svg> */}
+                    <svg ref={scrollRef} className='scroll' width='40' height='77' viewBox='0 0 40 77' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                        <g id='scroll' transform="translate(-253 -787)">
+                            <g id='Rectangle_1' data-name="Rectangle 1" transform="translate(253 787)" fill='none' stroke='white' strokeWidth='4'>
+                                <rect width='40' height='77' rx='20' stroke='none'/>
+                                <rect width='36' height='73' x='2' y='2' rx='18' fill='none'/>
+                            </g>
+                            <circle className='circle' id='Ellipse_1' data-name='Ellipse 1' cx='11' cy='11' r='11' transform="translate(262 798)" fill='#fff' />
+                        </g>
+                    </svg>
+
+                </div>
+                
+                {/* hero design svg */}
+                <svg ref={heroDesignRef} className='hero-design' width="639" height="640" viewBox="0 0 639 640" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M319 400C319 444.183 283.183 480 239 480C194.817 480 159 444.183 159 400C159 355.817 194.817 320 239 320C283.183 320 319 355.817 319 400Z" fill="#00F7FF" fillOpacity="0.65" shapeRendering="crispEdges"/>
+                    <path d="M480 240C480 284.183 444.183 320 400 320C355.817 320 320 284.183 320 240C320 195.817 355.817 160 400 160C444.183 160 480 195.817 480 240Z" fill="#FF64CB" fillOpacity="0.65"/>
+                    <rect x="161" y="160" width="160" height="160" fill="white"/>
+                    <rect x="161" y="480" width="160" height="160" fill="white"/>
+                    <rect x="319" width="160" height="160" fill="#6E00FF"/>
+                    <rect x="319" y="320" width="160" height="160" fill="#6E00FF"/>
+                    <rect y="320" width="160" height="160" fill="#FF64CB" fillOpacity="0.1"/>
+                    <rect x="480" y="160" width="160" height="160" fill="#6E00FF" fillOpacity="0.1"/>
+                </svg>
+
+            </div>
+
+            {/* Featured project */}
+            <section className='featured'>
+                <div className='left'>
+                    <div className='inner transition2'>
+                        <p className='subtitle'>Featured Project</p>
+                        <a href='#' className='title'>Automated Trading Platform</a>
+
+                        <p className='description'>
+                            Automated trading platform for stock market and cryptocurrency trading. Built with Rust, React, Node, Laravel, Kafka and MySQL.
+                        </p>
+                    </div>
+                </div>
+                <img className='right transition2' src='/images/featured.png' alt='Featured project' />
+            </section>
+
+            {/* Skills */}
+            <section className='skills'>
+                <div className='skills-container'>
+                    <ul>
+                        <li className='transition2'>
+                            <div className='icon-container one'>
+                                <img src='/images/icons/architect.svg' alt='System Architecture Icon' />
+                            </div>
+                            <p className='title'>System Architecture</p>
+                            <p className='description'>
+                                Transform Your Business with a Senior Engineer's Expertise. Discover top-notch system architecture in our portfolio. Benefit from optimized performance, scalability, and security. Partner with a seasoned developer today.
+                            </p>
+                        </li>
+                        <li className='transition2'>
+                            <div className='icon-container two'>
+                                <img src='/images/icons/frontend.svg' alt='Frontend Icon' />
+                            </div>
+                            <p className='title'>Frontend Development</p>
+                            <p className='description'>
+                                Elevate Your User Experience with Frontend Development Expertise. Explore our portfolio to see the results of seamless design and technical excellence. Partner with us to bring your vision to life and create an exceptional user experience.
+                            </p>
+                        </li>
+                        <li className='transition2'>
+                            <div className='icon-container three'>
+                                <img src='/images/icons/backend.svg' alt='Backend Icon' />
+                            </div>
+                            <p className='title'>Backend Development</p>
+                            <p className='description'>
+                                Unleash the Potential of Your Backend with Expert Development. Explore our portfolio and see the power of robust, scalable, and secure backend solutions. Partner with us to streamline your operations and drive your business forward.
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+
+            {/* Portfolio */}
+            <section className='portfolio'>
+                <div className='portfolio-container transition3'>
+                    <div className="portfolio-left">
+                        <div className="inner">
+                            <p className="subtitle">Guitar App</p>
+                            <p className="title">Fretastic.com</p>
+                            <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quas consectetur dolore, fugit quidem, nesciunt molestiae animi est facere esse, blanditiis vitae quos libero consequuntur velit. Nihil vitae hic quis!</p>
+                        </div>
+                    </div>
+                    <a href='#'><img src='images/fretty.jpg' alt='Fretastic' /></a>
+                </div>
+                <div className='portfolio-container transition3'>
+                    <div className="portfolio-left">
+                        <div className="inner">
+                            <p className="subtitle">Guitar App</p>
+                            <p className="title">Fretastic.com</p>
+                            <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quas consectetur dolore, fugit quidem, nesciunt molestiae animi est facere esse, blanditiis vitae quos libero consequuntur velit. Nihil vitae hic quis!</p>
+                        </div>
+                    </div>
+                    <a href='#'><img src='images/fretty.jpg' alt='Fretastic' /></a>
+                </div>
+                <div className='portfolio-container transition3'>
+                    <div className="portfolio-left">
+                        <div className="inner">
+                            <p className="subtitle">Guitar App</p>
+                            <p className="title">Fretastic.com</p>
+                            <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quas consectetur dolore, fugit quidem, nesciunt molestiae animi est facere esse, blanditiis vitae quos libero consequuntur velit. Nihil vitae hic quis!</p>
+                        </div>
+                    </div>
+                    <a href='#'><img src='images/fretty.jpg' alt='Fretastic' /></a>
+                </div>
+            </section>
+
+            {/* <section className={utilStyles.headingMd}>
+                <p>Building Beautifully Designed Software Solutions</p>
+            </section>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                {allPostsData.map(({ id, date, title }) => (
+                    <li className={utilStyles.listItem} key={id}>
+                    <Link href={`/posts/${id}`}>{title}</Link>
+                    <br />
+                    <small className={utilStyles.lightText}>
+                        <Date dateString={date} />
+                    </small>
+                    </li>
+                ))}
+                </ul>
+            </section> */}
+        </Layout>
+    )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
